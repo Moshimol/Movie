df9e0b944c32112c32aefccc13cdf4be09679de2
lushitong <269797304@qq.com>
2021-08-17 00:20:29 +0800

    feat:添加资源和tabbar

---
+++ Movie/miniprogram/components/tab-bar/tab-bar.js
@@ -0,0 +1,48 @@
+Component({
+  properties: {
+    selected: {
+      type: Number,
+      value: 0
+    },
+    placeholder: {
+      type: Boolean,
+      value: false
+    },
+    list: {
+      type: Array,
+      value: [
+        {
+          "pagePath": "/pages/tabs/discovery/discovery",
+          "text": "发现",
+          "iconPath": "/assets/images/tabbar/tabbar_discovery.png",
+          "selectedIconPath": "/assets/images/tabbar/tabbar_discovery_selected.png"
+        },
+        {
+          "pagePath": "/pages/tabs/movies/movies",
+          "text": "清单",
+          "iconPath": "/assets/images/tabbar/tabbar_list.png",
+          "selectedIconPath": "/assets/images/tabbar/tabbar_list_selected.png"
+        },
+        {
+          "pagePath": "/pages/tabs/index/index",
+          "text": "我的",
+          "iconPath": "/assets/images/tabbar/tabbar_mine.png",
+          "selectedIconPath": "/assets/images/tabbar/tabbar_mine_selected.png"
+        }
+      ]
+    }
+  },
+
+  data: {},
+
+  methods: {
+    handleSwitchTab (e) {
+      /** @type {{ index: number }} */
+      const { index } = e.currentTarget.dataset;
+      const { list } = this.data;
+      this.setData({ selected: index });
+      this.triggerEvent('change', { index, list });
+    }
+  }
+});
+ 
